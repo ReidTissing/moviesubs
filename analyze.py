a4a530953723fed57d566c042ce6001df16d2eb6
ReidTissing <reid.tissing@gmail.com>
2020-03-30 22:47:24 -0500

    updated parameters, removed uppercase words from getFrequency function

--- analyze.py
+++ analyze.py
@@ -4,14 +4,21 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
 
-def getFrequency(subfilepath, outputimg):
-    file = open(subfilepath, "rt")
 
+def getFrequency(moviename, subfilepath, outputimg):
+    file = open(subfilepath, "rt")
     text = file.read()
     file.close()
 
     # split into words
-    tokens = word_tokenize(text)
+    tokenized = word_tokenize(text)
+    # remove uppercase words, usually sounds
+    tokens = []
+    for token in tokenized:
+        if token.isupper():
+            print(token)
+        else:
+            tokens.append(token)
     # convert to lower case
     tokens = [w.lower() for w in tokens]
     # remove punctuation from each word
@@ -24,7 +31,7 @@ def getFrequency(subfilepath, outputimg):
     words = [w for w in words if not w in stop_words]
     junk_words = ['nt', 'get']
     words = [w for w in words if not w in junk_words]
-    #test print first 100 words
+    # test print first 100 words
     # print(words[:100])
 
     freqDist = FreqDist(words)
@@ -32,15 +39,15 @@ def getFrequency(subfilepath, outputimg):
 
     # print(freqDist.plot(10))
 
-    fig = plt.figure(figsize = (10,4))
-    plt.gcf().subplots_adjust(bottom=0.15) # to avoid x-ticks cut-off
+    fig = plt.figure(figsize=(10, 4))
+    plt.gcf().subplots_adjust(bottom=0.15)  # to avoid x-ticks cut-off
     plt.xlabel('words', fontsize=18)
     plt.ylabel('times said', fontsize=16)
     fdist = FreqDist(freqDist)
-    fdist.plot(10, cumulative=False, title="Most Frequently Used Words in Jurassic Park World")
+    fdist.plot(10, cumulative=False, title="Most Frequently Used Words in " + moviename)
     plt.show()
     # fig.suptitle('test title', fontsize=20)
-    fig.savefig(outputimg + '.png', bbox_inches = "tight")
+    fig.savefig(outputimg + '.png', bbox_inches="tight")
 
-getFrequency("D:\\code\\nltk\\subtitles\\jwworld.srt","jworld")
 
+getFrequency("Jurassic World Fallen Kingdom", "D:\\code\\nltk\\subtitles\\jwworld.srt", "jworld")
